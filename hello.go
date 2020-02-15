562da2018b214a4b9ef20feb79a1120c97336209
arakawan <arakawan@pivot.jp>
2020-02-16 00:00:29 +0900

    Lesson13

--- hello.go
+++ hello.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 func main() {
-	var s = "Hello World!"
-	//○番目のアルファベットを取り出すことができる
-	fmt.Println(string("HelloWorld!"[1]))
+	var x int = 1
+	xx := float64(x)
 
-	//条件に合致する初めの１回だけ文字を置き換えることができる
-	//実際のs自体は置き換わらないので注意
-	fmt.Println(strings.Replace(s, "H", "K", 1))
+	//型、値、小数
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
 
-	//直接sの中に入れることで文字を置き換えることができる
-	s = strings.Replace(s, "H", "X", 1)
-	fmt.Println(s)
-	fmt.Println("test\n" +
-		"test")
-	fmt.Println(`test
-test`)
+	//型、値、整数
+	var y = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d\n", yy, yy, yy)
 
+	//文字型を数値型に変換
+	var s string = "14"
+
+	//アンスコにすることで、エラーが表示されなくなる
+	i, _ := strconv.Atoi(s)
+	fmt.Printf("%T %v\n", i, i)
 }
