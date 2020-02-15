395cb9da1212bd694ffe0e2f31778c2d81a9a348
arakawan <arakawan@pivot.jp>
2020-02-16 01:09:08 +0900

    Lesson17

--- hello.go
+++ hello.go
@@ -3,17 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	m := map[string]int{"apple": 100, "orange": 200}
+	fmt.Println(m["apple"])
+	m["new"] = 400
+	fmt.Println(m)
 
-	//長さは3、キャパシティは5である、このままだと000が入る
-	n := make([]int, 3, 5)
+	//値が存在するかを確認できる
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
 
-	//長さはlen、キャパシティはcap
-	fmt.Printf("len=%d cap=%d value=%v", len(n), cap(n), n)
+	//値が存在するかを確認できる
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
 
-	c = make([]int, 0, 5)
-	for i := 0; i < 5; i++ {
-		c = append(c, i)
-		fmt.Println(c)
-	}
-	fmt.Println(c)
+	//mapの初期化
+	var m2 = make(map[string]int)
+	m2["banana"] = 200
+	fmt.Println(m2)
 }
