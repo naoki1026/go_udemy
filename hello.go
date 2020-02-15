0eb076cc79dd19e0750e9e68891f02485e437736
arakawan <arakawan@pivot.jp>
2020-02-15 22:58:10 +0900

    Lesson8

--- hello.go
+++ hello.go
@@ -1,16 +1,47 @@
 package main
 
-import (
-	"fmt"
-	"os/user"
-	"time"
-)
+import "fmt"
 
-func init() {
-	fmt.Println("Init")
-}
+// func main() {
+// 	var i int = 1
+// 	var f64 float64 = 1.2
+// 	var s string = "text"
+// 	var t bool = true
+// 	var f bool = false
+// 	fmt.Println(i, f64, s, t, f)
+// }
 
+//カッコでひとまとめにすることができる
+// func main() {
+// 	var (
+// 		i   int     = 1
+// 		f64 float64 = 1.2
+// 		s   string  = "text"
+// 		t   bool    = true
+// 		f   bool    = false
+// 	)
+// 	fmt.Println(i, f64, s, t, f)
+// }
+
+//初期化する場合
 func main() {
-	fmt.Println("Hello world!", time.Now())
-	fmt.Println(user.Current())
+	var (
+		i   int
+		f64 float64
+		s   string
+		t   bool
+		f   bool
+	)
+	fmt.Println(i, f64, s, t, f)
+
+	//関数内でしか宣言することができない
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt := true
+	xf := false
+	fmt.Println(xi, xf64, xs, xt, xf)
+
+	//変数の型を調べることができる
+	fmt.Printf("%T\n", xf64)
 }
