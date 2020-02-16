2c6b952ff526290cae022cb2c3f7e3cf3949c8e9
arakawan <arakawan@pivot.jp>
2020-02-16 18:01:40 +0900

    Lesson19

--- hello.go
+++ hello.go
@@ -2,22 +2,58 @@ package main
 
 import "fmt"
 
+// func add1(x int, y int) {
+// 	fmt.Println("add function")
+// 	fmt.Println(x + y)
+// }
+
+// //関数addを呼び出している
+// func main() {
+// 	add1(10, 20)
+// }
+
+// //返り値が１つの場合
+// func add2(x int, y int) int {
+// 	fmt.Println("add function")
+// 	return x + y
+// }
+
+// //関数addを呼び出している
+// func main() {
+// 	r := add2(10, 20)
+// 	fmt.Println(r)
+// }
+
+//返り値が2つの場合
+//返り値の型に対してカッコが必要になる
+func add3(x, y int) (int, int) {
+	fmt.Println("add function")
+	return x + y, x * y
+}
+
+//変数名を関数内で宣言して使用することができ、
+//returnの後のresultも省略することができる
+func cal(price, item int) (result int) {
+	result = price * item
+	return result
+}
+
+//関数addを呼び出している
 func main() {
-	m := map[string]int{"apple": 100, "orange": 200}
-	fmt.Println(m["apple"])
-	m["new"] = 400
-	fmt.Println(m)
+	r1, r2 := add3(10, 20)
+	fmt.Println(r1, r2)
 
-	//値が存在するかを確認できる
-	v, ok := m["apple"]
-	fmt.Println(v, ok)
+	r3 := cal(200, 3)
+	fmt.Println(r3)
 
-	//値が存在するかを確認できる
-	v2, ok2 := m["nothing"]
-	fmt.Println(v2, ok2)
+	//無名関数のようなもの
+	f := func() {
+		fmt.Println("inner func")
+	}
+	f()
 
-	//mapの初期化
-	var m2 = make(map[string]int)
-	m2["banana"] = 200
-	fmt.Println(m2)
+	//無名関数のようなもの2
+	func(num int) {
+		fmt.Println(num)
+	}(1)
 }
