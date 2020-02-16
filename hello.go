e0d696df67ec3e39c249b88b35ad71debc42662c
arakawan <arakawan@pivot.jp>
2020-02-16 18:30:04 +0900

    Lesson20

--- hello.go
+++ hello.go
@@ -2,58 +2,46 @@ package main
 
 import "fmt"
 
-// func add1(x int, y int) {
-// 	fmt.Println("add function")
-// 	fmt.Println(x + y)
-// }
-
-// //関数addを呼び出している
 // func main() {
-// 	add1(10, 20)
+// 	x := 0
+
+// 	//変数にfunctionを定義することができる
+// 	increment := func() int {
+// 		x++
+// 		return x
+// 	}
+// 	fmt.Println(increment())
+// 	fmt.Println(increment())
+// 	fmt.Println(increment())
 // }
 
-// //返り値が１つの場合
-// func add2(x int, y int) int {
-// 	fmt.Println("add function")
-// 	return x + y
-// }
-
-// //関数addを呼び出している
-// func main() {
-// 	r := add2(10, 20)
-// 	fmt.Println(r)
-// }
-
-//返り値が2つの場合
-//返り値の型に対してカッコが必要になる
-func add3(x, y int) (int, int) {
-	fmt.Println("add function")
-	return x + y, x * y
-}
-
-//変数名を関数内で宣言して使用することができ、
-//returnの後のresultも省略することができる
-func cal(price, item int) (result int) {
-	result = price * item
-	return result
-}
-
-//関数addを呼び出している
-func main() {
-	r1, r2 := add3(10, 20)
-	fmt.Println(r1, r2)
-
-	r3 := cal(200, 3)
-	fmt.Println(r3)
-
-	//無名関数のようなもの
-	f := func() {
-		fmt.Println("inner func")
+//返り値にfunctionを返すことができる
+func incrementGenerator() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
 	}
-	f()
+}
 
-	//無名関数のようなもの2
-	func(num int) {
-		fmt.Println(num)
-	}(1)
+func circleArea(pi float64) func(radius float64) float64 {
+	return func(radius float64) float64 {
+		return pi * radius * radius
+	}
+}
+
+func main() {
+
+	//変数にfunctionを入れることができる
+	//int型を返すことができる
+	counter := incrementGenerator()
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
+
+	c1 := circleArea(3.14)
+	fmt.Println(c1(3))
+
+	c2 := circleArea(3)
+	fmt.Println(c2(3))
 }
