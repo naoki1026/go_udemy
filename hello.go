002360dc8b964ab3a9f2d72c791259918fa9eea9
arakawan <arakawan@pivot.jp>
2020-02-16 21:20:31 +0900

    Lesson34 ポインタ

--- hello.go
+++ hello.go
@@ -2,23 +2,47 @@ package main
 
 import "fmt"
 
-func thirdPartyConnectDB(){
-	//panicは自分で何を修正すれば良いのかわからない時なので、
-	//そのようなことがないようにしておくことが大事である
-	panic("Unable to conenct to Database")
+// func main() {
+// 	var n int = 100
+// 	fmt.Println(n)
+
+// 	//100を入れたメモリのアドレスが表示される
+// 	fmt.Println(&n)
+
+// 	//*intはintのポイント型という意味である
+// 	var p *int = &n
+// 	fmt.Println(p)
+// 	fmt.Println(*p)
+// }
+
+// func one(x int){
+// 	x = 1
+// }
+
+// func main(){
+// 	var n int = 100
+
+// 	//nという値だけをoneという関数に渡している
+// 	//oneにはx=1と定義してあるが、影響していない
+// 	one(n)
+// 	fmt.Println(n)
+// }
+
+
+//1.ポイント型にはアスタリスクをつける
+//3.xのままだとアドレスを入れることができないため、
+//最終的にはデリファンレンス、実体の中に1を入れる
+//xはnのアドレスなので、その中に1を入れている
+func one(x *int){
+	*x = 1
 }
 
-func save(){
-	defer func(){
+func main(){
+	var n int = 100
 
-		//panicでシステムが強制終了しないようにrecoverしている
-		s := recover()
-		fmt.Println(s)
-		}()
-		thirdPartyConnectDB()
-	}
-
-func main() {
-	save()
-	fmt.Println("OK?")
+	//2.ポイント型にはアドレスを渡す必要があるため&をつける
+	//100のアドレスをここで渡している
+	one(&n)
+	fmt.Println(n)
+	fmt.Println(&*&n)
 }
