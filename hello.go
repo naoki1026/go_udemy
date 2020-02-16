9fb671cc92ce313964655c7f30d637f5c90ebe03
arakawan <arakawan@pivot.jp>
2020-02-16 20:23:30 +0900

    Lesson27 switch文

--- hello.go
+++ hello.go
@@ -2,30 +2,18 @@ package main
 
 import "fmt"
 
+func getOsName() string{
+	return "mac"
+
+}
+
 func main() {
-	l := []string{"phthon", "go", "java"}
-	for i := 0; i < len(l); i++ {
-		fmt.Println(i, l[i])
-	}
-
-	//lの中身を取ってきてindexと値を表示する
-	for i, v := range l {
-		fmt.Println(i, v)
-	}
-
-	//forをアンスコにすることでindexを表示させないこともできる
-	for _, v := range l {
-		fmt.Println(v)
-	}
-
-	m := map[string]int{"apple": 100, "banana" : 200}
-	for k, v := range m {
-		fmt.Println(k, v)
-	}
-	for k := range m {
-		fmt.Println(k)
-	}
-	for _, v := range m {
-		fmt.Println(v)
+	switch os := getOsName(); os{
+	case "mac":
+		fmt.Println("Mac!!")
+	case "windows":
+		fmt.Println("windows!!")
+	default:
+		fmt.Println("default")
 	}
 }
