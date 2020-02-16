c4b1398abf5561576fb3775db474c6ed4217f2c0
arakawan <arakawan@pivot.jp>
2020-02-16 19:00:43 +0900

    Lesson21

--- hello.go
+++ hello.go
@@ -2,46 +2,24 @@ package main
 
 import "fmt"
 
-// func main() {
-// 	x := 0
+//可変長引数
+//関数の定義時に取り決めた引数を増やすことができないため、...intを使用する
+func foo(params ...int) {
 
-// 	//変数にfunctionを定義することができる
-// 	increment := func() int {
-// 		x++
-// 		return x
-// 	}
-// 	fmt.Println(increment())
-// 	fmt.Println(increment())
-// 	fmt.Println(increment())
-// }
-
-//返り値にfunctionを返すことができる
-func incrementGenerator() func() int {
-	x := 0
-	return func() int {
-		x++
-		return x
-	}
-}
-
-func circleArea(pi float64) func(radius float64) float64 {
-	return func(radius float64) float64 {
-		return pi * radius * radius
+	//lenは長さ
+	fmt.Println(len(params), params)
+	for _, param := range params {
+		fmt.Println(param)
 	}
 }
 
 func main() {
+	foo()
+	foo(10, 20)
+	foo(10, 20, 30)
 
-	//変数にfunctionを入れることができる
-	//int型を返すことができる
-	counter := incrementGenerator()
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
+	s := []int{1, 2, 3}
+	fmt.Println(s)
 
-	c1 := circleArea(3.14)
-	fmt.Println(c1(3))
-
-	c2 := circleArea(3)
-	fmt.Println(c2(3))
+	foo(s...)
 }
