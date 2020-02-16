978029019a95a97a795942fc88803a85135b0ba6
arakawan <arakawan@pivot.jp>
2020-02-16 20:16:14 +0900

    Lesson26 range

--- hello.go
+++ hello.go
@@ -3,25 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i < 10; i++ {
-		fmt.Println(i)
-
-		//続ける
-		if i == 3 {
-			fmt.Println("continue")
-			continue
-		}
-
-		//ここでfor文がストップする
-		if i == 6 {
-			fmt.Println("break")
-			break
-		}
-
+	l := []string{"phthon", "go", "java"}
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
 	}
-	sum := 1
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
+
+	//lの中身を取ってきてindexと値を表示する
+	for i, v := range l {
+		fmt.Println(i, v)
+	}
+
+	//forをアンスコにすることでindexを表示させないこともできる
+	for _, v := range l {
+		fmt.Println(v)
+	}
+
+	m := map[string]int{"apple": 100, "banana" : 200}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	for k := range m {
+		fmt.Println(k)
+	}
+	for _, v := range m {
+		fmt.Println(v)
 	}
 }
